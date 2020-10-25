7ec1f83d0e3a9b4c17226ed1d160c722079d8781
datadiode <datadiode@users.noreply.github.com>
2020-10-25 10:49:57 +0100

    Formula Parser: Assume en-us locale

--- ReoGrid/Formula/Parser.cs
+++ ReoGrid/Formula/Parser.cs
@@ -21,6 +21,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using unvell.ReoGrid.IO.OpenXML;
 
 namespace unvell.ReoGrid.Formula
 {
@@ -51,16 +52,8 @@ namespace unvell.ReoGrid.Formula
 			return node;
 		}
 
-		private static string ParameterSeparator = ",";
+		private const string ParameterSeparator = ",";
 
-		static Parser()
-		{
-			try
-			{
-				ParameterSeparator = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ListSeparator;
-			}
-			catch { }
-		}
 		#endregion // Parser API
 
 		#region Parsers
@@ -479,7 +472,8 @@ namespace unvell.ReoGrid.Formula
 				case STNodeType.NUMBER:
 					string text = lexer.Input.Substring(start, len);
 					double v = 0;
-					return double.TryParse(text, out v) ? new STNumberNode(v, start, len) : null;
+					return double.TryParse(text, System.Globalization.NumberStyles.Number,
+						ExcelWriter.EnglishCulture, out v) ? new STNumberNode(v, start, len) : null;
 
 				case STNodeType.IDENTIFIER:
 					return new STIdentifierNode(lexer.Cell == null ? null : lexer.Cell.Worksheet, lexer.Input.Substring(start, len), start, len);
@@ -528,8 +522,7 @@ namespace unvell.ReoGrid.Formula
 			"\\s*((?<string>\"(?:\"\"|[^\"])*\")|(?<union_ranges>[A-Z]+[0-9]+:[A-Z]+[0-9]+(\\s[A-Z]+[0-9]+:[A-Z]+[0-9]+)+)"
 			+ "|(?<range>\\$?[A-Z]+\\$?[0-9]*:\\$?[A-Z]+\\$?[0-9]*)"
 			+ "|(?<cell>\\$?[A-Z]+\\$?[0-9]+)"
-			+ "|(?<token>-)|(?<number>\\-?\\d*\\"
-			+ System.Threading.Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator + "?\\d+)"
+			+ "|(?<token>-)|(?<number>\\-?\\d*\\.?\\d+)"
 			+ "|(?<true>(?i)TRUE)|(?<false>(?i)FALSE)|(?<identifier>\\w+)"
 			+ "|(?<token>\\=\\=|\\<\\>|\\<\\=|\\>\\=|\\<\\>|\\=|\\!|[\\=\\.\\,\\+\\-\\*\\/\\%\\<\\>\\(\\)\\&\\^]))",
 			RegexOptions.Compiled);
