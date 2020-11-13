2615df34b273d27655c984d4518b8b8e88fd3f9d
datadiode <datadiode@users.noreply.github.com>
2020-11-13 07:04:58 +0100

    Apply Zoltu.Versioning to all projects included in Demo.sln

--- Editor/Properties/AssemblyInfo.cs
+++ Editor/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f16dd6fe-47b0-4159-870e-53eaa59f8428")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
