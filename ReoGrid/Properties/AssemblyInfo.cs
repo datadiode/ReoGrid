9a8441b0a78a7998c9553971231b0bbd23415ec0
datadiode <datadiode@users.noreply.github.com>
2021-05-09 10:58:13 +0200

    ReoGrid: Remove Resources/seek.dat (no idea what it was good for) / Update EULA_EN.txt / Update AssemblyInfo / Cleanups

--- ReoGrid/Properties/AssemblyInfo.cs
+++ ReoGrid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReoGrid")]
-[assembly: AssemblyDescription(".NET Spreadsheet Control")]
+[assembly: AssemblyDescription("Powerful and lightweight open source .NET spreadsheet component. Compatible with Excel 2007 format or later. Simple APIs to open/edit/save/print an Excel spreadsheet in the .NET application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("unvell.com")]
+[assembly: AssemblyCompany("unvell")]
 [assembly: AssemblyProduct("ReoGrid")]
-[assembly: AssemblyCopyright("Copyright © 2012-2020 unvell.com, All Rights Seserved.")]
+[assembly: AssemblyCopyright("Copyright © 2012-2021 Jingwood & unvell, all rights reserved.")]
 [assembly: AssemblyTrademark("ReoGrid.NET")]
 [assembly: AssemblyCulture("")]
 
